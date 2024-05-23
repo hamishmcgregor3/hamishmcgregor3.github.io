88b78858fde45ad7b0d757fa8fbc703da66a4666
hamishmcgregor3 <35543569+hamishmcgregor3@users.noreply.github.com>
2024-05-22 22:14:23 -0500

    updating routing + flashing lamp animation for web

--- script.js
+++ script.js
@@ -100,4 +100,56 @@ document.addEventListener("DOMContentLoaded", function () {
       // Start the flickering effect
       flicker();
     });
+
+    document
+    .getElementById("lamp-img-web")
+    .addEventListener("click", function (event) {
+      
+      event.preventDefault(); 
+      const lampImg = document.getElementById("lamp-img-web");
+      const images = ["/images/click_here.png", "/images/click_here_hover.png"];
+
+      function toggleImageSrc(img) {
+        const currentSrc = img.src.split("/").pop();
+        if (currentSrc === images[0].split("/").pop()) {
+          return images[1];
+        } else {
+          return images[0];
+        }
+      }
+
+      let intervalDuration = 400; // Start with a 1-second interval
+      const totalDuration = 1500; // Total duration of 4 seconds
+      const intervalDecrement = 100; // Decrease interval by 100ms each time
+      const minIntervalDuration = 100; // Minimum interval duration of 100ms
+
+      let elapsedTime = 0;
+
+      function flicker() {
+        if (elapsedTime >= totalDuration) {
+          // Set the image source to _hover
+          lampImg.src = images[1];
+          // Navigate to the next page
+          setTimeout(function () {
+            window.location.href = "/pages/menu/";
+          }, 300);
+          return;
+        }
+
+        lampImg.src = toggleImageSrc(lampImg);
+        elapsedTime += intervalDuration;
+
+        // Decrease the interval duration to increase speed
+        intervalDuration = Math.max(
+          minIntervalDuration,
+          intervalDuration - intervalDecrement
+        );
+
+        setTimeout(flicker, intervalDuration);
+      }
+
+      // Start the flickering effect
+      flicker();
+    });
+
 });
